6d6a8d4d208b7f8b389112de6db95a5381b2ff59
Jonas Melo <jonasmelo@ciandt.com>
2019-07-07 05:23:41 -0300

    prototipo componente pessoas proximas

--- src/components/PessoasProximas.vue
+++ src/components/PessoasProximas.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
+<div>
+  <div class="text-h5" style="text-color=secondary;">Pessoas próximas a você</div>
+  <div class="row big-gutter">
+    <div>
+      <q-avatar class="avatarPessoaProxima" size="100px" font-size="52px" color="teal" text-color="white">
+          <img src="https://randomuser.me/api/portraits/men/93.jpg">
+      </q-avatar>
+      <div class="text-center text-bold">Jonas Melo</div>
+      <div class="text-center">CI&T</div>
+      <q-btn class="botao" outline style="color: secondary;" label="Enviar Mensagem" />
+    </div>
 
+      <div v-for="x in 4" :key="x">
+      <q-avatar class="avatarPessoaProxima" size="100px" font-size="52px" color="teal" text-color="white">
+          <img src="https://randomuser.me/api/portraits/men/24.jpg">
+      </q-avatar>
+      <div class="text-center text-bold">Eduardo Bonovolenta</div>
+      <div class="text-center">Thoughtworks</div>
+      <q-btn class="botao" color="secondary" label="Conectar" />
+    </div>
   </div>
+</div>
 </template>
 
-<style></style>
+<style>
+.avatarPessoaProxima {
+  margin-left:35px
+}
+.botao {
+  min-width:180px
+}
+</style>
 
 <script>
 export default {
