32d2ed924b5c131bd5e602f9187eb5b35b034ddc
Hermógenes <hermogenes@Hermogeness-MacBook-Pro.local>
2019-07-07 06:04:33 -0300

    Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sidebar q-pa-lg">
+        <div class="row justify-start items-center">
+            <div class="col-auto">
+                <q-avatar size="64px" font-size="52px" color="teal" text-color="white">
+                    <img src="https://randomuser.me/api/portraits/men/93.jpg">
+                </q-avatar>
+            </div>
+            <div class="col-auto">
+                <h6 class="text-teal-8 text-weight-bold q-ma-none q-pl-lg">Zé das couves</h6>
+            </div>
+        </div>
+    </div>
+</template>
