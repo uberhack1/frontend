66b11f3d92078e9350321f39c57aaa494eb1b7c6
Hermógenes <hermogenes@Hermogeness-MacBook-Pro.local>
2019-07-07 06:23:43 -0300

    Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,8 +7,21 @@
                 </q-avatar>
             </div>
             <div class="col-auto">
-                <h6 class="text-teal-8 text-weight-bold q-ma-none q-pl-lg">Zé das couves</h6>
+                <h6 class="text-teal-9 text-weight-bold q-ma-none q-pl-lg">Zé das couves</h6>
+                <p class="q-pl-lg text-caption text-teal-7 q-ma-none">VER MEU PERFIL</p>
             </div>
+        </div>
+        <div class="row justify-start items-center q-pt-lg">
+            <div class="col-auto"><q-icon name="place" class="text-teal-9" style="font-size: 20px"></q-icon></div>
+            <div class="col-auto"><span class="q-pl-md text-teal-9">Mora em <strong>Belo Horizonte</strong></span></div>
+        </div>
+        <div class="row justify-start items-center q-pt-sm">
+            <div class="col-auto"><q-icon name="work" class="text-teal-9" style="font-size: 20px"></q-icon></div>
+            <div class="col-auto"><span class="q-pl-md text-teal-9">Trabalha no <strong>Google</strong></span></div>
+        </div>
+        <div class="row justify-start items-center q-pt-sm">
+            <div class="col-auto"><q-icon name="school" class="text-teal-9" style="font-size: 20px"></q-icon></div>
+            <div class="col-auto"><span class="q-pl-md text-teal-9">Estuda na <strong>UniBH Buritis</strong></span></div>
         </div>
     </div>
 </template>
