a96deb76d8f394936aa973ef734b9a2f3badcb89
Eduardo Bovolenta <eduardoalbuquerque10@hotmail.com>
2019-07-07 05:51:20 -0300

    Componente grupos sugeridos

---
+++ src/components/GruposSugeridos.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <h3 class="q-pa-md titulo-componente">Grupos Sugeridos</h3>
+    <div class="row q-pa-md q-gutter-md">
+      <q-card flat bordered class="my-card col meu-card">
+        <q-card-section>
+          <div class="text-h6">Faculdade Pitágoras</div>
+        </q-card-section>
+
+        <q-card-section class="color-grey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+
+        <q-card-section>
+          <div class="q-pa-md q-gutter-sm">
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar color="grey-13" size="40px" font-size="12px" text-color="white">+22</q-avatar>
+          </div>
+        </q-card-section>
+        <q-card-section class="color-teal">ENTRAR NO GRUPO →</q-card-section>
+      </q-card>
+
+      <q-card flat bordered class="my-card col">
+        <q-card-section>
+          <div class="text-h6">Faculdade Pitágoras</div>
+        </q-card-section>
+
+        <q-card-section class="color-grey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+
+        <q-card-section>
+          <div class="q-pa-md q-gutter-sm">
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar>
+              <img src="https://i.ibb.co/vZrvbxn/pp.jpg" />
+            </q-avatar>
+            <q-avatar color="grey-13" size="40px" font-size="12px" text-color="white">+22</q-avatar>
+          </div>
+        </q-card-section>
+        <q-card-section class="color-teal">ENTRAR NO GRUPO →</q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus">
+.color-grey {
+  color: $grey-7;
+}
+
+.text-h6 {
+  color: $teal-9;
+}
+
+.color-teal {
+  color: $teal-10;
+  font-weight: 700;
+}
+
+.meu-card {
+  min-width: 300px;
+}
+
+.titulo-componente
+  color: $teal-10
+  margin-bottom: 0px !important;
+  margin-top: 0px !important;
+</style>
+
+
